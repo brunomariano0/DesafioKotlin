d8bd99256d00a2a21d0e59da98b81570f4a2202d
Bruno Mariano <40373544+brunomariano0@users.noreply.github.com>
2020-12-08 14:49:14 -0300

    Add files via upload

--- Main.kt
+++ Main.kt
@@ -4,14 +4,24 @@ fun main() {
 
     val executar = DigitalHouseManager()
 
-    //Registro dos Professores
+    //Registro dos Professores Titular
 
     executar.registrarProfessorTitular("João","Silva",45,"Android")
     executar.registrarProfessorTitular("Felipe","Silva",25,"Android")
+
+    println("Registro dos professores titulares realizado com sucesso")
+    // Não sei dá quebra de linha no kotlin, esse foi o jeito que eu arrumei para deixar a leitura melhor.
+    println("")
+
+    // Registro dos Professores Adjutos
+
     executar.registrarProfessorAdjunto("Fernando","Ferreira",12,12)
     executar.registrarProfessorAdjunto("Lucas","Ferreira",22,65)
 
-    println("Registro dos professores foi efetuado com sucesso")
+    println("Registro dos professores Adjuntos realizado com sucesso")
+    // Não sei dá quebra de linha no kotlin, esse foi o jeito que eu arrumei para deixar a leitura melhor.
+    println("")
+
 
 
     // Registro dos Cursos
@@ -19,7 +29,10 @@ fun main() {
     executar.registrarCurso("Full Stack",20001,3)
     executar.registrarCurso("Android",20002,2)
 
-    println("Registro dos curso Android e Full Stack foi efetuado com sucesso")
+    println("Registro do curso Full Stack e Android foi efetuado com sucesso")
+    // Não sei dá quebra de linha no kotlin, esse foi o jeito que eu arrumei para deixar a leitura melhor.
+    println("")
+
 
 
     //Alocar Professor Titular e Adjunto em cada curso
@@ -28,6 +41,9 @@ fun main() {
     executar.alocarProfessores(20001,25,22)
 
     println("Professor foi alocado no curso com sucesso")
+    // Não sei dá quebra de linha no kotlin, esse foi o jeito que eu arrumei para deixar a leitura melhor.
+    println("")
+
 
     //Matricular Alunos
 
@@ -38,6 +54,11 @@ fun main() {
     executar.matricularAluno("Ayrton","Silva",654,)
     executar.matricularAluno("Diego","Ferreira",987,)
 
+    println("Matricula do alunos feita com sucesso")
+    // Não sei dá quebra de linha no kotlin, esse foi o jeito que eu arrumei para deixar a leitura melhor.
+    println("")
+
+
 
 
     // Matricular dois alunos no curso Full Stack
@@ -46,12 +67,20 @@ fun main() {
     executar.matricularAlunonoCurso(456,20001)
 
     println("Aluno Matriculado com sucesso no curso de Full Stack")
+    // Não sei dá quebra de linha no kotlin, esse foi o jeito que eu arrumei para deixar a leitura melhor.
+    println("")
+
 
     //Matricular três alunos no curso de Android
 
     executar.matricularAlunonoCurso(321,20002)
     executar.matricularAlunonoCurso(654,20002)
     executar.matricularAlunonoCurso(987,20002)
+
+    println("Alunos matriculado com sucesso no curso android")
+    // Não sei dá quebra de linha no kotlin, esse foi o jeito que eu arrumei para deixar a leitura melhor.
+    println("")
+
 
 
 }
